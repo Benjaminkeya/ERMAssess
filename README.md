d2b7961162bb342eeb328297108cc10755df78ff
Benjaminkeya <benjaminkeya93@gmail.com>
2025-01-27 16:28:46 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -4,13 +4,10 @@ Run "npm install cypress --save-dev"
 Open npx cypress open
 
 Install: ## all dependencies
-Run "npm i" to instal all cypress dependency packages
+Run "npm i" to instal all cypress dependencies
 Run tests: ## Runs all e2e tests
-//Specify foldeer to run spec  files from
-run "npx cypress run -P  hsi/test" to run all tests in the spec files
-
 npm run tests
- 
+
 
 
 
--- cypress.config.js
+++ cypress.config.js
@@ -1,53 +1,63 @@
 const { defineConfig } = require("cypress");
 const fs = require('fs');
-const {verifyDownloadTasks} =require('cy-verify-downloads');
+const pdf = require('pdf-parse');
+const path = require('path')
+const csv = require('csv-parser');
+
+const { verifyDownloadTasks } = require('cy-verify-downloads');
 const { cypressBrowserPermissionsPlugin } = require('cypress-browser-permissions');
-//const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+const { randomUUID } = require("crypto");
 module.exports = defineConfig({
+  fileServerFolder: 'cypress/fixtures',
   projectId: 'isqitv',
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    charts: true,
-    reportPageTitle: 'custom-title',
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    saveAllAttempts: false,
-  },
+  // reporter: 'cypress-mochawesome-reporter',
+  // reporterOptions: {
+  //   reportDir: 'cypress/reports',
+  //   charts: true,
+  //   reportPageTitle: 'custom-title',
+  //   embeddedScreenshots: true,
+  //   inlineAssets: true,
+  //   saveAllAttempts: false,
+  //   html:true
+  //   // json:false
+  // },
   //Experimental test flake Detection and allow retries
 retries: {
-  experimentalStrategy: 'detect-flake-and-pass-on-threshold',
+  experimentalStrategy:'detect-flake-and-pass-on-threshold',
   experimentalOptions: {
-    maxRetries: 2,
+    maxRetries: 1,
     passesRequired: 1,
+    },
+    // you must also explicitly set openMode and runMode to
+    // either true or false when using experimental retries
+    openMode: true,
+    runMode: true,
   },
-  // you must also explicitly set openMode and runMode to
-  // either true or false when using experimental retries
-  openMode: true,
-  runMode: true,
-},
   chromeWebSecurity: false,
-  defaultCommandTimeout: 10000,
-  pageLoadTimeout: 8000,
-  requestTimeout: 8000,
+  defaultCommandTimeout: 30000,
+  pageLoadTimeout: 30000,
+  requestTimeout: 30000,
   watchForFileChanges: false,
   scrollBehavior: false,
   video: false,
   viewportWidth: 1280,
   viewportHeight: 720,
+
   env:{
       testrail: {
         domain: "https://pixeledgeqateam.testrail.io",
         username: "benjamin.keya@pixeledge.io",
         password: "Benjamin@1",
-        projectId: "",
+        projectId: "1",
         milestoneId: "",
-        suiteId: "",
+        suiteId: "1",
         runId:"",
-        runName: "Cypress regression",
+        runName: "ERMAssess January 2025 Automated Regression run",
         runIncludeAll: "",
         closeRun: false,
-        screenshots: "",
-        screenshotsAll: true
+        screenshots: true,
+        screenshotsAll: false,
+        ignorePending:true
       } ,
       browserPermissions: {
         notifications: "allow",
@@ -60,14 +70,13 @@ retries: {
         baseUrl:"https://www.ermassess.com",
         deBaseUrl:"https://de.ermassess.com/api/v1"
       }
-    },
+  },
   e2e: {
     setupNodeEvents(on, config) {
       config = cypressBrowserPermissionsPlugin(on, config)
-      //allureWriter(on, config)
-      require('cypress-mochawesome-reporter/plugin')(on);
-       //spec pattern here
-       config.specPattern = [
+     // require('cypress-mochawesome-reporter/plugin')(on);
+      //spec pattern here
+      config.specPattern = [
         'cypress/e2e/Login.cy.js',
         'cypress/e2e/Organization.cy.js',
         'cypress/e2e/Dashboard.cy.js',
@@ -75,20 +84,59 @@ retries: {
         'cypress/e2e/Entities.cy.js',
         'cypress/e2e/Portfolio.cy.js',
         'cypress/e2e/Assessments.cy.js',
-        'cypress/e2e/Geotags.cy.js',
+        'cypress/e2e/AuditQuestions.cy.js',
         'cypress/e2e/ActionItems.cy.js',
-        'cypress/e2e/Notifications.cy.js',
         'cypress/e2e/DataExport.cy.js',
         'cypress/e2e/ActivityLog.cy.js',
         'cypress/e2e/ManageAccount.cy.js',
+       //'cypress/e2e/Roles.cy.js',
         'cypress/e2e/Logout.cy.js',
-      ]
-       // implement node event listeners here
-       on('task',verifyDownloadTasks)
-      // const TestRailReporter = require('cypress-testrail');
-      // new TestRailReporter(on, config).register();
+        
+        ]
+      // implement node event listeners here
+      on('task', verifyDownloadTasks)
+      //const customComment = 'AUT v' + Cypress.env('MY_APP_VERSION');
+      const TestRailReporter = require('cypress-testrail');
+      new TestRailReporter(on, config).register();
+
+      // Register the 'readPdf' task
+      on('task',
+        {
+          readPdf(pdfPath) {
+            return new Promise((resolve) => {
+              const filePath = path.resolve(pdfPath)
+              const dataBuffer = fs.readFileSync(filePath)
+              pdf(dataBuffer).then(function (data) {
+                resolve(data);
+              })
+            })
+          }
+        })
+
+      // Register the 'readCsv' task
+      on('task', {
+        readCsv(filePath) {
+
+          return new Promise((resolve, reject) => {
+            const absolutePath = path.join(config.projectRoot, filePath);
+
+            const rows = [];
+            fs.createReadStream(absolutePath)
+              .pipe(csv())
+              .on('data', (row) => {
+                rows.push(row);
+              })
+              .on('end', () => {
+                resolve({ rows });
+              })
+              .on('error', (error) => {
+                reject(error);
+              });
+          });
+        },
+      });
+
       return config
     }
-  }
+  },
 });
-
